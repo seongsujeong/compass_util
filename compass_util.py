dcac8ae7c062e8c54356b0390ed5b5d9192b80ea
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-01-18 08:02:54 -0800

    initial commit

---
+++ compass_util.py
@@ -0,0 +1,225 @@
+'''
+Collection of routines for validation of COMPASS GSLC products
+
+'''
+import numpy as np
+import isce3
+from osgeo import osr, gdal
+import h5py
+import glob
+import os
+from pyproj import CRS, Transformer
+import csv
+import matplotlib.pyplot as plt
+
+
+def extract_slc(path_slc_in: str, path_amp_out: str, pol: str='VV',
+                to_amplitude = False, to_mem=False):
+    '''Convert SLC into single-band ampliture image '''
+
+    # TODO: Implement to apply multilook
+    with h5py.File(path_slc_in, 'r') as hin:
+        arr_slc = np.array(hin[f'/science/SENTINEL1/CSLC/grids/{pol}'])
+        x_coords = np.array(hin['/science/SENTINEL1/CSLC/grids/x_coordinates'])
+        y_coords = np.array(hin['/science/SENTINEL1/CSLC/grids/y_coordinates'])
+        str_wkt_projection = str(hin['/science/SENTINEL1/CSLC/grids/projection'].\
+                                 attrs['spatial_ref'].decode())
+
+    option_gtiff = ['COMPRESS=LZW', 'BIGTIFF=YES']
+
+    ncols = arr_slc.shape[1]
+    nrows = arr_slc.shape[0]
+    # Calculate geotransformation parameters
+    x_start = x_coords[0]
+    y_start = y_coords[0]
+
+    spacing_x = (x_coords[-1] - x_coords[0]) / (ncols-1)
+    spacing_y = (y_coords[-1] - y_coords[0]) / (nrows-1)
+
+    if to_mem:
+        drv_out = gdal.GetDriverByName('MEM')
+    else:
+        drv_out = gdal.GetDriverByName('GTiff')
+
+
+    if to_amplitude:
+        raster_out = drv_out.Create(path_amp_out, ncols, nrows,
+                                1, gdal.GDT_Float32, option_gtiff)
+        raster_out.WriteArray(np.abs(arr_slc))
+    else:
+        raster_out = drv_out.Create(path_amp_out, ncols, nrows,
+                                1, gdal.GDT_CFloat32, option_gtiff)
+        raster_out.WriteArray(arr_slc)
+
+    raster_out.SetGeoTransform((x_start, spacing_x, 0,
+                                y_start, 0, spacing_y))
+    raster_out.SetProjection(str_wkt_projection)
+
+    raster_out.FlushCache()
+
+    if to_mem:
+        return raster_out
+    else:
+        # de-reference the raster object
+        raster_out = None
+        return None
+
+
+def slc_to_amplitude_batch(topdir_slc_in: str, dir_amp_out: str):
+    '''
+    Search the list of SLC files. Convert them into amplitude images
+    '''
+
+    # TODO: Parallelize the processing
+
+    list_slc_in = glob.glob(f'{topdir_slc_in}/**/*.h5', recursive=True)
+    num_slc_in = len(list_slc_in)
+    for i_slc, slc_in in enumerate(list_slc_in):
+        print(f'Processing: {i_slc+1} of {num_slc_in}')
+        filename_out = os.path.basename(slc_in).replace('.h5','.tif')
+        path_amp = f'{dir_amp_out}/{filename_out}'
+        extract_slc(slc_in, path_amp, to_amplitude=True)
+
+
+
+def form_interferogram(path_slc_ref:str, path_slc_sec:str, path_ifg_out:str):
+    '''
+    Form interferogram using ISCE3
+    '''
+    pass
+
+
+
+def mosaic_raster(list_raster_in: str, path_raster_mosaic: str):
+    '''
+    Mosaic the input rasters
+    '''
+    pass
+
+
+
+def load_cr_coord_csv(path_cr_csv):
+    '''Load the corner reflectors' coordinates in CSV,
+        provided from JPL UAVSAR webpage
+
+    '''
+    lines_csv = []
+    with open(path_cr_csv, 'r') as csv_in:
+        csv_reader = csv.reader(csv_in)
+        for line_csv in csv_reader:
+            lines_csv.append(line_csv)
+
+    return lines_csv
+
+
+def is_hdf5(path_data):
+    '''
+    Chech if `path_data` is HDF5 file.
+    '''
+    try:
+        hin = h5py.File(path_data, 'r')
+        hin.close()
+        return True
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f'File not found: {path_data}')
+
+    except OSError:
+        return False
+
+
+def extract_slc_coord_cr(path_slc, latlon_cr, is_gslc=True, ovs_factor = 128, window_size = 128):
+    '''
+    Extract the corner reflectors' coordinates
+
+    path_slc: Path to the input SLC to find CR
+    latlon_cr: Latitute and longitude of CR
+    is_gslc: True if the input SLC is geocoded; False otherwise
+    ovs_factor: Oversampling factor
+
+    '''
+
+    if is_hdf5(path_slc):
+        raster_in = extract_slc(path_slc, path_slc+'.slc', 'VV', False, True)
+    else:
+        raster_in = gdal.Open(path_slc, gdal.GA_ReadOnly)
+
+
+    arr_slc = raster_in.ReadAsArray()
+    proj_slc = raster_in.GetProjection()
+    if is_gslc:
+        geotransform_slc = raster_in.GetGeoTransform()
+    else:
+        geotransform_slc = (0.0, 1.0, 0.0, 0.0, 0.0, 1.0)
+
+    # Done with the input SLC raster. de-reference it before forgetting it
+    raster_in = None
+
+    # convert the CR's coordinates (in geographic) into map coordinates
+    crs_from = CRS.from_epsg(4326)
+    crs_to = CRS.from_wkt(proj_slc)
+
+    transformer_coord = Transformer.from_crs(crs_from, crs_to, always_xy=True)
+    xy_cr = transformer_coord.transform(latlon_cr[1], latlon_cr[0])
+
+
+    # Calculate the image coord. of CR using its map coord. and geotransformation
+    x_img = (xy_cr[0] - geotransform_slc[0])/geotransform_slc[1]
+    y_img = (xy_cr[1] - geotransform_slc[3])/geotransform_slc[5]
+
+
+    # check if the x_img and y_img are within the input SLC
+    upperleft_x = int(np.round(x_img)) - window_size//2
+    upperleft_y = int(np.round(y_img)) - window_size//2
+    lowerright_x = upperleft_x + window_size
+    lowerright_y = upperleft_y + window_size
+
+    if (upperleft_x < 0) or (upperleft_y < 0) or \
+       (lowerright_x > arr_slc.shape[1]) or (lowerright_y > arr_slc.shape[0]):
+        raise ValueError('The input coordinates of CR is out of the input SLC.')
+
+    # Extract the image chip from source SLC
+    slc_sub = arr_slc[upperleft_y:lowerright_y, upperleft_x:lowerright_x]
+    #plt.imshow(np.abs(slc_sub))
+    #plt.show()
+
+
+    #find the peak
+    slc_ov =  isce3.signal.point_target_info.oversample(slc_sub, ovs_factor)
+    idx_peak_ovs = np.argmax(slc_ov)
+    imgxy_peak_ovs = np.unravel_index(idx_peak_ovs, slc_ov.shape)
+
+    imgxy_peak = (upperleft_y + imgxy_peak_ovs[0]/ovs_factor,
+                  upperleft_x + imgxy_peak_ovs[1]/ovs_factor)
+
+    mapx_peak = geotransform_slc[0] + imgxy_peak[1]*geotransform_slc[1]
+    mapy_peak = geotransform_slc[3] + imgxy_peak[0]*geotransform_slc[5]
+
+
+
+    return (mapx_peak, mapy_peak)
+
+
+
+if __name__=='__main__':
+    # put test codes here
+    '''path_slc = ('/Users/jeong/Documents/'
+                'OPERA_SCRATCH/CSLC/CSLC_BETA_DELIVERY/'
+                'CSLC_outputs_from_different_envs/'
+                'output_s1_cslc.aurora.2022_1221/'
+                't064_135518_iw1/20220501/t064_135518_iw1_20220501_VV.h5')
+
+    path_amp = ('/Users/jeong/Desktop/amp.slc')
+    
+    slc_to_amplitude(path_slc, path_amp)'''
+
+    # Test code #1 : batch conversion of GSLC to amplitude GEOTIFF
+    #path_in = '/u/aurora-r0/jeong/scratch/CSLC/stack_processing_Rosamond/output_s1_cslc_no_correction'
+    #path_out = '/u/aurora-r0/jeong/scratch/CSLC/stack_processing_Rosamond/amplitude_backsctter_no_correction'
+    #slc_to_amplitude_batch(path_in, path_out)
+
+    # Test code #2: extract the CR coordinates from GSLC
+    PATH_GSLC_H5 = ('/Users/jeong/Documents/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_no_correction/t064_135523_iw2/20220922/t064_135523_iw2_20220922_VV.h5')
+
+    extract_slc_coord_cr(PATH_GSLC_H5, [34.8054937, -118.0708031], True, 32)
+
---
+++ s1_cslc_parallel.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+import multiprocessing
+import glob
+import s1reader
+import subprocess
+import os
+import yaml
+import argparse
+import time
+
+dict_pol_to_load = {
+    '1SDV':'VV',
+    '1SSV':'VV',
+    '1SVV':'VV',
+    '1SDH':'HH',
+    '1SSH':'HH',
+    '1SHH':'HH',
+    '1SVH':'VH',
+    '1SHV':'HV',
+}
+
+def get_all_burst_id(path_safe):
+    '''
+    Docstring here
+    '''
+    # determine the polarization to load
+    # determination Strategy: Use co-pol as much as possible;
+    #                         Load cross-pol only if necessary
+
+    str_pp_safe = os.path.basename(path_safe).split('_')[4]
+    pol = dict_pol_to_load[str_pp_safe]
+
+    list_burst_id = []
+
+    for i_subswath in range(3):
+        bursts_subswath = s1reader.load_bursts(path_safe, None, i_subswath+1, pol)
+        list_burst_id_subswath = [str(burst.burst_id) for burst in bursts_subswath]
+        list_burst_id += list_burst_id_subswath
+
+    burst_id_set = set(list_burst_id)
+
+    return burst_id_set
+
+
+def find_common_bursts(list_safe_zip):
+    '''
+    Docstring here
+    '''
+    num_safe_zip = len(list_safe_zip)
+
+    # initial set
+    set_common_burst = get_all_burst_id(list_safe_zip[0])
+
+    for i_safe in range(1, num_safe_zip):
+        print(f'Taking a look {i_safe} / {num_safe_zip}')
+        set_burst_id_safe = get_all_burst_id(list_safe_zip[i_safe])
+        set_common_burst = set_common_burst & set_burst_id_safe
+
+    return list(set_common_burst)
+
+
+def spawn_runconfig(arg_in):
+    '''
+    Split the input runconfig into single burst runconfigs.
+    Writes out the runconfigs.
+    Return the list of the burst runconfigs.
+
+    Parameters:
+
+    Returns:
+    list_runconfig_burst: list(str)
+        List of the burst runconfigs
+    list_logfile_burst: list(str)
+        List of the burst logfiles,
+        which corresponds to `list_runconfig_burst`
+    '''
+    with open(arg_in.run_config_path, 'r+', encoding='utf8') as fin:
+        runconfig_dict_ref = yaml.safe_load(fin.read())
+    scratch_path = runconfig_dict_ref['runconfig']['groups']['product_path_group']['scratch_path']
+
+    list_safe = glob.glob(f'{args.dir_safe}/S1*.zip')
+
+    list_burst_id = runconfig_dict_ref['runconfig']['groups']['input_file_group']['burst_id']
+    if list_burst_id is None:
+        list_burst_id = find_common_bursts(list_safe)
+
+    os.makedirs(arg_in.dir_orbit, exist_ok=True)
+    os.makedirs(scratch_path, exist_ok=True)
+
+    runconfig_burst_list = []
+
+    for path_safe in list_safe:
+        str_date = os.path.basename(path_safe).split('_')[5].split('T')[0]
+        # TODO improve the readabilit of the line above
+        list_burst_in_safe = get_all_burst_id(path_safe)
+        for burst_id in list_burst_id:
+            if not burst_id in list_burst_in_safe:
+                continue
+
+            path_temp_runconfig = os.path.join(scratch_path,
+                                            f'burst_runconfig_{burst_id}_{str_date}.yaml')
+
+            runconfig_dict_out = runconfig_dict_ref.copy()
+
+            runconfig_dict_out['runconfig']['groups']['input_file_group']['safe_file_path'] = [path_safe]
+            path_orbit = s1reader.get_orbit_file_from_dir(path_safe,
+                                                          arg_in.dir_orbit,
+                                                          auto_download=True)
+
+            runconfig_dict_out['runconfig']['groups']['input_file_group']['orbit_file_path'] = [path_orbit]
+            runconfig_dict_out['runconfig']['groups']['input_file_group']['burst_id'] = [burst_id]
+
+            runconfig_burst_list.append(path_temp_runconfig)
+
+            with open(path_temp_runconfig, 'w+', encoding='utf8') as fout:
+                yaml.dump(runconfig_dict_out, fout)
+
+    return runconfig_burst_list
+
+
+
+def get_parser():
+    '''Initialize YamlArgparse class and parse CLI arguments for OPERA RTC.
+    Modified after copied from `rtc_s1.py`
+    '''
+    parser = argparse.ArgumentParser(description='',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('run_config_path',
+                        type=str,
+                        default=None,
+                        help='Path to run config file')
+
+    parser.add_argument('dir_safe',
+                        type=str,
+                        default=None,
+                        help='Directory for the safe file')
+
+    parser.add_argument('-o',
+                        dest='dir_orbit',
+                        type=str,
+                        default='orbits',
+                        help='Directory for orbit')
+
+    # Determine the default # of concurrent workers
+    ncpu_default = os.cpu_count()
+    if os.getenv('OMP_NUM_THREADS') is not None:
+        omp_num_threads = int(os.getenv('OMP_NUM_THREADS'))
+        ncpu_default = min(ncpu_default, omp_num_threads)
+
+    parser.add_argument('-n',
+                        dest='num_workers',
+                        type=int,
+                        default=ncpu_default,
+                        help='Number of concurrent workers.')
+
+    parser.add_argument('--log',
+                        '--log-file',
+                        dest='log_file',
+                        type=str,
+                        help='Log file')
+
+    parser.add_argument('--full-log-format',
+                        dest='full_log_formatting',
+                        action='store_true',
+                        default=False,
+                        help='Enable full formatting of log messages')
+
+    return parser
+
+
+
+
+def process_runconfig(path_runconfig_burst):
+    '''
+    single worker to process runconfig from terminal using `subprocess`
+
+    Parameters:
+    path_runconfig_burst: str
+        Path to the burst runconfig
+    path_logfile_burst: str
+        Path to the burst logfile
+    full_log_format: bool
+        Enable full formatting of log messages.
+        See `get_rtc_s1_parser()`
+
+    '''
+
+    list_arg_subprocess = ['s1_cslc.py', path_runconfig_burst]
+
+    rtnval = subprocess.run(list_arg_subprocess)
+
+    # TODO Add some routine to take a look into `rtnval` to see if everything is okay.
+
+    os.remove(path_runconfig_burst)
+
+
+def process_frame_parallel(arg_in):
+    '''
+    Take in the parsed arguments from CLI,
+    split the original runconfign into bursts,
+    and process them concurrently
+
+    Parameter:
+    arg_in: Namespace
+        Parsed argument. See `get_rtc_s1_parser()`
+    '''
+
+    t0 = time.time()
+    list_burst_runconfig = spawn_runconfig(arg_in)
+
+    with multiprocessing.Pool(arg_in.num_workers) as p:
+        p.map(process_runconfig, list_burst_runconfig)
+
+    t1 = time.time()
+
+    print(f'elapsed time: {t1-t0:06f} seconds.')
+
+
+if __name__=='__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    process_frame_parallel(args)
+    #spawn_runconfig(args)
+
