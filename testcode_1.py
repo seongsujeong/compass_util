7c28b3fc8a33e4bb892e304a6a21692b8e63874e
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-02-04 02:50:19 -0800

    mod on `testcode_1.py` to adapt the change

--- testcode_1.py
+++ testcode_1.py
@@ -18,28 +18,29 @@ if __name__=='__main__':
     path_slc_rg_only = '/Users/jeong/Documents/OPERA_SCRATCH/CSLC/LUT_CORRECTION_TEST_SITE/output_s1_cslc_rg_correction_only/t064_135523_iw2/20221016/t064_135523_iw2_20221016_VV.h5'
     latlon_cr = [34.80549368, -118.070803]
 
-    rt_no_corr = cu.extract_slc_coord_cr(path_slc_no_corr,
+
+    rt_no_corr = cu.extract_gslc_coord_cr(path_slc_no_corr,
                               latlon_cr,
-                              128, 32, 
+                              128, 32, 'VV',
                               path_fig=f'{path_slc_no_corr}.png',
                               verbose=True)
 
-    rt_all_corr = cu.extract_slc_coord_cr(path_slc_all_corr,
+    rt_all_corr = cu.extract_gslc_coord_cr(path_slc_all_corr,
                               latlon_cr,
-                              128, 32,
+                              128, 32, 'VV',
                               path_fig=f'{path_slc_all_corr}.png',
                               verbose=True)
 
-    rt_az_only = cu.extract_slc_coord_cr(path_slc_az_only,
+    rt_az_only = cu.extract_gslc_coord_cr(path_slc_az_only,
                               latlon_cr,
-                              128, 32,
+                              128, 32, 'VV',
                               path_fig=f'{path_slc_az_only}.png',
                               verbose=True,
                               )
 
-    rt_rg_only = cu.extract_slc_coord_cr(path_slc_rg_only,
+    rt_rg_only = cu.extract_gslc_coord_cr(path_slc_rg_only,
                               latlon_cr,
-                              128, 32,
+                              128, 32, 'VV',
                               path_fig=f'{path_slc_rg_only}.png',
                               verbose=True)
 
