e9aac595cd65cfae696421c6dd2b60b954553a8d
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-02-04 04:04:41 -0800

    changes in`testcode_3.py`

--- testcode_3.py
+++ testcode_3.py
@@ -16,8 +16,8 @@ latlon_cr = [34.80549368, -118.070803] # CR05
 
 dict_1 = cu.extract_slc_coord_cr_stack_parallel(path_stack_no_correction, latlon_cr, 128, 32, 'VV', True, 8)
 with open(path_json_no_correction, 'w+') as jout1:
-    json.dump(dict_1, jout1)
+    json.dump(dict_1, jout1, indent=2)
 
 dict_2 = cu.extract_slc_coord_cr_stack_parallel(path_stack_with_correction, latlon_cr, 128, 32, 'VV', True, 8)
 with open(path_json_with_correction, 'w+') as jout2:
-    json.dump(dict_2, jout2)
+    json.dump(dict_2, jout2, indent=2)
