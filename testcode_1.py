8695903cbd4b951b413779115f90b90ee534dff5
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-02-04 05:27:00 -0800

    updates on testcodes

--- testcode_1.py
+++ testcode_1.py
@@ -1,4 +1,4 @@
-''' Test code - 1
+'''' Test code - 1
 Description:
 - Load the GSLCs in the path specified in the main namespace below,
 - extract the CR coordinates from the GSLCs
@@ -7,7 +7,7 @@ Description:
 
 import compass_util as cu
 import matplotlib.pyplot as plt
-
+import json
 
 
 ## test code
@@ -16,6 +16,7 @@ if __name__=='__main__':
     path_slc_all_corr = '/Users/jeong/Documents/OPERA_SCRATCH/CSLC/LUT_CORRECTION_TEST_SITE/output_s1_cslc_all_correction/t064_135523_iw2/20221016/t064_135523_iw2_20221016_VV.h5'
     path_slc_az_only = '/Users/jeong/Documents/OPERA_SCRATCH/CSLC/LUT_CORRECTION_TEST_SITE/output_s1_cslc_az_correction_only/t064_135523_iw2/20221016/t064_135523_iw2_20221016_VV.h5'
     path_slc_rg_only = '/Users/jeong/Documents/OPERA_SCRATCH/CSLC/LUT_CORRECTION_TEST_SITE/output_s1_cslc_rg_correction_only/t064_135523_iw2/20221016/t064_135523_iw2_20221016_VV.h5'
+    path_geojeon_out = '/Users/jeong/Documents/OPERA_SCRATCH/CSLC/LUT_CORRECTION_TEST_SITE/CR_DETECTION_RESULT.geojeon'
     latlon_cr = [34.80549368, -118.070803]
 
 
@@ -43,6 +44,90 @@ if __name__=='__main__':
                               128, 32, 'VV',
                               path_fig=f'{path_slc_rg_only}.png',
                               verbose=True)
+    # writeout the result into json
+    dict_out = {
+        "type": "FeatureCollection",
+        "name": "t064_135523_iw2_20221016_VV",
+        "crs": {
+            "type": "name",
+            "properties": {
+                "name": "urn:ogc:def:crs:EPSG::32611"
+            }
+        },
+        "features": []
+    }
+
+
+    dict_feat_CR = {
+        "type": "Feature",
+        "properties": {
+            "NAME": "CR05"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                rt_no_corr[2], rt_no_corr[3]
+            ]
+        }
+    }
+
+    dict_feat_no_corr = {
+        "type": "Feature",
+        "properties": {
+            "NAME": "NO_CORRECTION"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                rt_no_corr[0], rt_no_corr[1]
+            ]
+        }
+    }
+
+    dict_feat_all_corr = {
+        "type": "Feature",
+        "properties": {
+            "NAME": "ALL_CORRECTION"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                rt_all_corr[0], rt_all_corr[1]
+            ]
+        }
+    }
+
+    dict_feat_az_only = {
+        "type": "Feature",
+        "properties": {
+            "NAME": "AZIMUTH_ONLY"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                rt_az_only[0], rt_az_only[1]
+            ]
+        }
+    }
+
+    dict_feat_rg_only = {
+        "type": "Feature",
+        "properties": {
+            "NAME": "RANGE_ONLY"
+        },
+        "geometry": {
+            "type": "Point",
+            "coordinates": [
+                rt_rg_only[0], rt_rg_only[1]
+            ]
+        }
+    }
+
+    dict_out['features'] = [dict_feat_CR,
+                            dict_feat_no_corr,
+                            dict_feat_all_corr,
+                            dict_feat_az_only,
+                            dict_feat_rg_only]
 
     #plot the results
     plt.plot(0,0,'ko',markersize=8) # CR
@@ -56,3 +141,6 @@ if __name__=='__main__':
     plt.grid()
     plt.axis('equal')
     plt.show()
+
+    with open(path_geojeon_out, 'w') as fout:
+        json.dump(dict_out, fout, indent=2)
--- testcode_4.py
+++ testcode_4.py
@@ -10,10 +10,10 @@ import compass_util as cu
 path_json = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_no_correction.json'
 path_shp = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_no_correction.shp'
 epsg_coord = 32611
-cu.json_to_shp(path_json, path_shp, epsg_coord)
+cu.stack_json_to_shp(path_json, path_shp, epsg_coord)
 
 
 path_json = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_with_correction_rev4.json'
 path_shp = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_with_correction_rev4.shp'
 epsg_coord = 32611
-cu.json_to_shp(path_json, path_shp, epsg_coord)
+cu.stack_json_to_shp(path_json, path_shp, epsg_coord)
