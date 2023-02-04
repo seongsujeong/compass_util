dc3d7bd7fe8039f9e552728c863e5c3d106c6a48
Seongsu Jeong <seongsu.jeong@jpl.nasa.gov>
2023-02-04 02:51:18 -0800

    updates in `testcode_4.py`

--- testcode_4.py
+++ testcode_4.py
@@ -4,7 +4,16 @@ To test out converting json (of CR detection results) into QGIS-readable format
 
 import compass_util as cu
 
-path_json = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/dict_with_correction_rev4.json'
-path_shp = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/dict_with_correction_rev4.shp'
+#path_json = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/dict_with_correction_rev4.json'
+#path_shp = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/dict_with_correction_rev4.shp'
+
+path_json = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_no_correction.json'
+path_shp = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_no_correction.shp'
+epsg_coord = 32611
+cu.json_to_shp(path_json, path_shp, epsg_coord)
+
+
+path_json = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_with_correction_rev4.json'
+path_shp = '/Volumes/HENRY_SSD/OPERA_SCRATCH/CSLC/STACK_PROCESSING/output_s1_cslc_with_correction_rev4.shp'
 epsg_coord = 32611
 cu.json_to_shp(path_json, path_shp, epsg_coord)
